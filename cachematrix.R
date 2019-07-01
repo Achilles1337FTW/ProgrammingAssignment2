a1e97df0102d6cb97715296a23944ed24e2e52aa
Achilles1337FTW <52363377+Achilles1337FTW@users.noreply.github.com>
2019-07-01 13:01:27 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Just like the makeVector function in the example, and given the argument of x=matrix()
+## I just swap out the m for i to represent 'inverse' and change all the "mean" with "inverse" and it should be exactly what I need
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y){
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Similar to the Cache Solve in the second example, I replace all the m's with i's since that's what I did in the CacheMatrix func
+## Then I replace all the mean's with inverse's since we're looking for a matrix inverse now
+## Lastly I changed line 31 from "m <- mean(data, ...)" to "i <- solve(data, ...)" since we want to solve for the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
